0e93579fbfa52fef2fa0f0710dd867088226a4c5
Skyli144 <143712942+Skyli144@users.noreply.github.com>
2026-04-19 22:26:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,11 +37,11 @@ function renderPage(pageId, btn) {
         title.innerText = "Mi Guía Personal";
         content.innerHTML = `
             <div class="personal-card">
-                <h3>💎 Top Acciones Recomendadas</h3>
-                <p>Análisis en tiempo real de los activos con mayor proyección y solidez en el mercado actual.</p>
+                <h3>💎 Selección Dinámica de Activos</h3>
+                <p>Gráficos actualizados al segundo de las 5 acciones con mayor solidez actual.</p>
             </div>
             <div class="widget-wrapper" style="height: 500px;">
-                <iframe src="https://s.tradingview.com/embed-widget/market-overview/?colorTheme=dark&showChart=true&locale=es&tabs=%5B%7B%22title%22%3A%22Acciones%22%2C%22symbols%22%3A%5B%7B%22s%22%3A%22NASDAQ%3AAPL%22%7D%2C%7B%22s%22%3A%22NASDAQ%3ANVDA%22%7D%2C%7B%22s%22%3A%22NASDAQ%3AMSFT%22%7D%2C%7B%22s%22%3A%22NASDAQ%3AAMZN%22%7D%2C%7B%22s%22%3A%22NASDAQ%3AGOOGL%22%7D%2C%7B%22s%22%3A%22NASDAQ%3AMETA%22%7D%5D%7D%5D" 
+                <iframe src="https://s.tradingview.com/embed-widget/market-overview/?colorTheme=dark&showChart=true&locale=es&tabs=%5B%7B%22title%22%3A%22Acciones%22%2C%22symbols%22%3A%5B%7B%22s%22%3A%22NASDAQ%3AAPL%22%7D%2C%7B%22s%22%3A%22NASDAQ%3ANVDA%22%7D%2C%7B%22s%22%3A%22NASDAQ%3AMSFT%22%7D%2C%7B%22s%22%3A%22NASDAQ%3AAMZN%22%7D%2C%7B%22s%22%3A%22NASDAQ%3AGOOGL%22%7D%5D%7D%5D" 
                         width="100%" height="100%" frameborder="0"></iframe>
             </div>
         `;
